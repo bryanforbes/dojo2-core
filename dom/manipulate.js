cb8f869af7d2143b0e6db7a114ae81b8c421fb2c
Bryan Forbes <bryan@reigndropsfall.net>
2013-01-08 12:59:21 -0600

    Simplify create().

--- dom/manipulate.js
+++ dom/manipulate.js
@@ -281,87 +281,18 @@ define([
 	 *
 	 * @param {string} tag
 	 * @param {Object=} attrs
-	 * @param {(Element|string)=} refNode
-	 * @param {string=} pos
+	 * @param {Document=} targetDocument
 	 */
-	exports.create = function create(tag, attrs, refNode, pos) {
-		// summary:
-		//		Create an element, allowing for optional attribute decoration
-		//		and placement.
-		// description:
-		//		A DOM Element creation function. A shorthand method for creating a node or
-		//		a fragment, and allowing for a convenient optional attribute setting step,
-		//		as well as an optional DOM placement reference.
-		//
-		//		Attributes are set by passing the optional object through `dojo.setAttr`.
-		//		See `dojo.setAttr` for noted caveats and nuances, and API if applicable.
-		//
-		//		Placement is done via `dojo.place`, assuming the new node to be the action
-		//		node, passing along the optional reference node and position.
-		// tag: DOMNode|String
-		//		A string of the element to create (eg: "div", "a", "p", "li", "script", "br"),
-		//		or an existing DOM node to process.
-		// attrs: Object
-		//		An object-hash of attributes to set on the newly created node.
-		//		Can be null, if you don't want to set any attributes/styles.
-		//		See: `dojo.setAttr` for a description of available attributes.
-		// refNode: DOMNode|String?
-		//		Optional reference node. Used by `dojo.place` to place the newly created
-		//		node somewhere in the dom relative to refNode. Can be a DomNode reference
-		//		or String ID of a node.
-		// pos: String?
-		//		Optional positional reference. Defaults to "last" by way of `dojo.place`,
-		//		though can be set to "first","after","before","last", "replace" or "only"
-		//		to further control the placement of the new node relative to the refNode.
-		//		'refNode' is required if a 'pos' is specified.
-		// example:
-		//		Create a DIV:
-		//	|	var n = dojo.create('div');
-		//
-		// example:
-		//		Create a DIV with content:
-		//	|	var n = dojo.create('div', { innerHTML:'<p>hi</p>' });
-		//
-		// example:
-		//		Place a new DIV in the BODY, with no attributes set
-		//	|	var n = dojo.create('div', null, dojo.body());
-		//
-		// example:
-		//		Create an UL, and populate it with LI's. Place the list as the first-child of a
-		//		node with id='someId':
-		//	|	var ul = dojo.create('ul', null, 'someId', 'first');
-		//	|	var items = ['one', 'two', 'three', 'four'];
-		//	|	dojo.forEach(items, function(data){
-		//	|		dojo.create('li', { innerHTML: data }, ul);
-		//	|	});
-		//
-		// example:
-		//		Create an anchor, with an href. Place in BODY:
-		//	|	dojo.create('a', { href:'foo.html', title:'Goto FOO!' }, dojo.body());
-		//
-		// example:
-		//		Create a `dojo/NodeList()` from a new element (for syntactic sugar):
-		//	|	dojo.query(dojo.create('div'))
-		//	|		.addClass('newDiv')
-		//	|		.onclick(function(e){ console.log('clicked', e.target) })
-		//	|		.place('#someNode'); // redundant, but cleaner.
+	exports.create = function (tag, props, targetDocument) {
+		targetDocument = targetDocument || document;
 
-		var doc = document;
-		if (refNode) {
-			refNode = dom.byId(refNode);
-			doc = refNode.ownerDocument;
+		var node = targetDocument.createElement(tag);
+
+		if (props) {
+			// TODO: figure out property setting
 		}
-		if (typeof tag === 'string') { // inline'd type check
-			tag = doc.createElement(tag);
-		}
-		// TODO: Figure out how we want to do attribute/property assignment
-		/*if (attrs) {
-			attr.set(tag, attrs);
-		}*/
-		if (refNode) {
-			exports.place(tag, refNode, pos);
-		}
-		return tag; // DomNode
+
+		return node;
 	};
 
 	function _empty(node) {
